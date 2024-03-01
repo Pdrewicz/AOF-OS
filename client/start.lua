0d8076f3cf2e76e560c5ac02f608aa226c3f7fa5
Pdrewicz <106173218+Pdrewicz@users.noreply.github.com>
2024-03-01 13:48:34 +0100

    a

--- client/start.lua
+++ client/start.lua
@@ -15,34 +15,6 @@ end
 downloadFile(url.."json.lua","json.lua")
 os.loadAPI("json.lua")
 
-local temp = json.decodeFromFile("settings.json")
-local testing = temp["testing"]
-if not testing then
-    url2 = url
-end
-
-if testing or (arg[1] and arg[1] == "update") then
-    downloadFile(url2.."os.lua","os.lua")
-    local leatestVersion = http.get(url2.."version.txt")
-    if leatestVersion then 
-        leatestVersion = tonumber(leatestVersion.readAll())
-        local settings = json.decodeFromFile("settings.json")
-        settings["version"] = leatestVersion
-        local file = fs.open("settings.json","w")
-        file.write(json.encodePretty(settings))
-        file.close()
-    end
-end
-
-downloadFile(url.."basalt.lua","basalt.lua")
-
-if not fs.exists("programs.json") then
-    local file = fs.open("programs.json","w")
-    local programs = {programs={}}
-    file.write(json.encodePretty(programs))
-    file.close()
-end
-
 if not fs.exists("settings.json") then
     local file = fs.open("settings.json","w")
     local settings = {
@@ -75,6 +47,34 @@ else
     end
     local file = fs.open("settings.json","w")
     file.write(json.encodePretty(settings))
+    file.close()
+end
+
+local temp = json.decodeFromFile("settings.json")
+local testing = temp["testing"]
+if not testing then
+    url2 = url
+end
+
+if testing or (arg[1] and arg[1] == "update") or not fs.exists("os.lua") then
+    downloadFile(url2.."os.lua","os.lua")
+    local leatestVersion = http.get(url2.."version.txt")
+    if leatestVersion then 
+        leatestVersion = tonumber(leatestVersion.readAll())
+        local settings = json.decodeFromFile("settings.json")
+        settings["version"] = leatestVersion
+        local file = fs.open("settings.json","w")
+        file.write(json.encodePretty(settings))
+        file.close()
+    end
+end
+
+downloadFile(url.."basalt.lua","basalt.lua")
+
+if not fs.exists("programs.json") then
+    local file = fs.open("programs.json","w")
+    local programs = {programs={}}
+    file.write(json.encodePretty(programs))
     file.close()
 end
 
